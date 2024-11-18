504479929ac7a2f54314fce73187d3df0d555768
AndriiLupoi <lupoi_2005@icloud.com>
2024-11-18 02:38:24 +0200

    finish

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap';
 
 const app = createApp(App)
 app.use(router)
